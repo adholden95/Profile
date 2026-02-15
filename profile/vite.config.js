34dcedc4d27196adcaf6f889380107b789116b72
adholden95 <adholden@live.co.uk>
2026-02-15 21:50:14 +0000

    Contact Page and Form
    contact page and form

--- profile/vite.config.js
+++ profile/vite.config.js
@@ -1,7 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
+
 })
